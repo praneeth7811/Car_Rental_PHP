0bfb8e6d0e6258bc247935e477ee7f599e27e3ea
Praneeth kumar <praneethkumarch5@gmail.com>
2024-03-06 01:17:56 +0530

    main

---
+++ connection.php
@@ -0,0 +1,17 @@
+<!-- <?php
+
+function Connect()
+{
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "carrentalp";
+	$dbport = 3307;
+
+	//Create Connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport) or die($conn->connect_error);
+
+	return $conn;
+}
+?> -->
+
